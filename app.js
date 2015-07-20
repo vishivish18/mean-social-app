e3266f818c474fb527b079f00f33a84108844a51
Vishal R <vishivish18@gmail.com>
2015-07-20 23:55:26 +0530

    post end point created, req.body.username returning undefined

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ app.get('/api/posts', function (req, res) {
 app.listen(3000, function () {
   console.log('App listening at ',3000);
 });
+
+
+app.post('/api/posts',function(req,res){
+console.log('post received');
+console.log(req.body.username);
+console.log(req.body.body);
+res.sendStatus(201);
+})
+
