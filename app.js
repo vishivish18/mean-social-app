6cadd239115a60741f2a46c79bf85ca06df021c4
Vishal R <vishivish18@gmail.com>
2015-07-21 23:48:35 +0530

    Post request working !

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 app.get('/api/posts', function (req, res) {
   res.json([
