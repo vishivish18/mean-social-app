1832b9f87dbe64203821a627707e77e8a48f273f
Vishal R <vishivish18@gmail.com>
2015-07-22 23:14:49 +0530

    Post working !

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var express = require('express');
+var Post = require('./models/post')
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-app.get('/api/posts', function (req, res) {
-  res.json([
-  {
-  	username : "vishalRanjan",
-  	body : "this is from node"
-  }
-  ])
+app.get('/api/posts', function (req, res, next) {
+  Post.find(function(err,posts){
+
+  	/*if(err) {return next(err)}*/
+  	if(err) {res.json([{error:"this is a error"}])}
+  	res.json(posts);
+
+  })
+
+
 });
 
 app.listen(3000, function () {
@@ -19,10 +23,16 @@ app.listen(3000, function () {
 });
 
 
-app.post('/api/posts',function(req,res){
-console.log('post received');
-console.log(req.body.username);
-console.log(req.body.body);
-res.sendStatus(201);
+app.post('/api/posts',function(req,res,next){
+var post = new Post({
+	username : req.body.username,
+	body : req.body.body
+
+})
+post.save(function (err,post){
+	if(err) {return next(err)}
+	res.json(201,post)
+
+	})	
 })
 
