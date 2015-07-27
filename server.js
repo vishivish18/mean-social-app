007ad1cedabfc9f4b280155e9c8131115d098ef6
Vishal R <vishivish18@gmail.com>
2015-07-28 00:02:59 +0530

    bcrypt working

--- server.js
+++ server.js
@@ -2,18 +2,39 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var jwt = require('jwt-simple')
+var _ = require('lodash')
+var bcrypt =require('bcrypt')
+
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 //app.use(require('body-parser').json)
 
+
+var users = [{username:'vishivish18',password:'$2a$10$xHr5GHJlavPXGJrw0EvGG.ZGQdju3s7boiViovILkg8YGUGBaz13m'}]
 var secretkey = "supersecretkey"
 
-app.post('/session', function(req,res){
-var username = req.body.username
-console.log(username);
+function findUserByUsername(username){
+	return _.find(users,{username:username})
+}
 
-var token = jwt.encode({username:username},secretkey)
-res.json(token)
+function validateUser(user,password,cb){
+
+	return bcrypt.compare(password,user.password,cb)
+
+
+}
+
+
+app.post('/session', function(req,res){
+var user = findUserByUsername(req.body.username)
+	validateUser(user, req.body.password, function(err,valid){
+		if(err || !valid) {return res.send(401)}
+			var token = jwt.encode({username: user.username},secretkey)
+			res.json(token+" "+" User is authenticated")
+
+
+	})
 })
 
 app.get('/user',function(req,res){
