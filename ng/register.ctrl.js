bafbc4e4c90775dcf266a4550d063db1d1948a2b
Vishal R <vishivish18@gmail.com>
2015-08-01 01:00:05 +0530

    temp layouts for login and home built, routing remaining

--- ng/register.ctrl.js
+++ ng/register.ctrl.js
@@ -3,7 +3,7 @@ angular.module('app')
 	$scope.register = function(username,password){
 		UserSvc.register(username,password)
 		.then(function(response){
-			
+			alert('Thank you for signing up '+ response.data.username)
 		})
 		.catch(function (err){
 			console.log(err)
