271b573c651b00e73e4da35e214cc2a97d07c51d
Vishal R <vishivish18@gmail.com>
2015-08-04 22:53:57 +0530

    Websockets on new post working

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,5 +1,6 @@
 var router = require('express').Router()
 var Post = require('../../models/post')
+var websockets = require('../../websockets')
 
 router.get('/', function (req, res, next) {  
   Post.find()
@@ -15,6 +16,7 @@ router.post('/', function (req, res, next) {
   post.username = req.auth.username // req.auth.username not working, undfined as of now
   post.save(function (err, post) {
     if (err) { return next(err) }
+    websockets.broadcast('new_post',post)
     res.status(201).json(post)
   })
 })
