7b1f279f437741bd6fe9a6b2a487904e3ef170b3
Vishal R <vishivish18@gmail.com>
2015-08-09 21:20:49 +0530

    checking login response

--- ng/login.ctrl.js
+++ ng/login.ctrl.js
@@ -3,6 +3,8 @@ angular.module('app')
 	$scope.login = function(username,password){
 		UserSvc.login(username,password)
 		.then(function(response){
+			console.log("printing response")
+			console.log(response.data)
 			$scope.$emit('login',response.data)
 			
 		})
