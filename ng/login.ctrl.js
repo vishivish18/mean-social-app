82c0ef2422bf54880d10f3b3706825f124fdee02
Vishal R <vishivish18@gmail.com>
2015-08-12 21:56:43 +0530

    Automated post creation also working now

--- ng/login.ctrl.js
+++ ng/login.ctrl.js
@@ -1,11 +1,12 @@
 angular.module('app')
-.controller('LoginCtrl',function($scope,UserSvc){
+.controller('LoginCtrl',function($scope,UserSvc,$location){
 	$scope.login = function(username,password){
 		UserSvc.login(username,password)
 		.then(function(response){
 			console.log("printing response")
 			console.log(response.data)
 			$scope.$emit('login',response.data)
+			$location.path('/')
 			
 		})
 	}
