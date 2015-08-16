12bee595f86fa1f88ff6a9bd5858967eee7e861d
Vishal R <vishivish18@gmail.com>
2015-08-16 22:22:50 +0530

    Checking express middlewares for routes

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var express = require('express');
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
 var Post = require('./models/post')
 var bodyParser = require('body-parser');
 var app = express();
