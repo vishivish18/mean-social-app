325cada1459cd25c8ed0b98826a5b21e9182c875
Vishal R <vishivish18@gmail.com>
2015-08-20 22:54:16 +0530

    Routes for redirection, middleware needs to be written

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 var express = require('express');
 var router = express.Router();              // get an instance of the express Router
 
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-router.get('/test', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-
-
 
 var Post = require('./models/post')
 var bodyParser = require('body-parser');
 var app = express();
+
 app.use('/api/route', router);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
